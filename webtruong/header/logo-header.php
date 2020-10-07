6eb3aeabf0a4342aac12187035adb0704aa3f996
Hiimcech <kiencech@gmail.com>
2020-10-07 23:08:05 +0700

    Toi them file header

---
+++ webtruong/header/logo-header.php
@@ -0,0 +1,8 @@
+<div class="logo">
+           
+              <div class="md-col-12">
+                  <img src="header/images/logo.jpg" alt="image-logo">
+                 <a href="index.php"></a>
+              </div>
+           
+ </div>
